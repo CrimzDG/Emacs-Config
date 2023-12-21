d44763d512e10da82a9cd77baeaead90e5ff2c37
George Mican <119901423+CrimzDG@users.noreply.github.com>
2023-12-21 12:46:22 +0000

    Update config with some more new packages and relocate and rename file
    Need to get this properly finished with use-package so its easy to carry around

--- .init.el
+++ .init.el
@@ -5,11 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruvbox-dark-soft))
  '(custom-safe-themes
-   '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" default))
- '(org-agenda-files '("~/Desktop/maynooth-year-3/semester1ToDo.org"))
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" default))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(org-modern org-appear lsp-pyright all-the-icons-ivy-rich all-the-icons-ivy all-the-icons ivy-rich counsel vterm ivy lsp-treemacs company-box company org-roam dired-rainbow
-		rainbow-delimiters paredit dired-sidebar eat lsp-mode paradox magit consult vertico use-package gruvbox-theme ##))
+   '(doom-themes doom-modeline dashboard page-break-lines projectile beacon nerd-icons all-the-icons-dired org-modern org-appear lsp-pyright all-the-icons-ivy-rich all-the-icons-ivy all-the-icons ivy-counsel rich vterm ivy lsp-treemacs company-box company org-roam dired-rainbow rainbow-delimiters paredit dired-sidebar eat lsp-mode paradox magit consult vertico use-package gruvbox-theme ##))
  '(paradox-automatically-star t)
  '(paradox-github-token t))
 
@@ -19,7 +18,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-hide ((t (:foreground "#32302f")))))
+ '(org-hide ((t (:foreground "#32302f"))))
+ '(org-modern-done-face ((t (:foreground "#8ec07c" :box (:line-width 2 :style released-button))))))
 (setq mac-option-key-is-meta nil
       mac-command-key-is-meta t
       mac-command-modifier 'meta
@@ -28,7 +28,10 @@
 
 (require 'org)
 (setq org-todo-keywords
-  '((sequence "TODO" "IN-PROGRESS" "CORRECTING" "DONE")))
+      '((sequence "TODO" "IN-PROGRESS" "CORRECTING" "DONE")))
+
+(setq org-modern-todo-faces
+       '(("DONE" . (:foreground "#3c3836" :background "#8ec07c" :weight bold))))
 
 (global-set-key (kbd "s-3") '(lambda () (interactive) (insert "#")))
 (global-set-key (kbd "C-c a") #'org-agenda)
@@ -45,6 +48,8 @@
 
 (add-hook 'org-mode-hook 'org-indent-mode)
 (add-hook 'org-mode-hook 'auto-fill-mode)
+
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -125,7 +130,16 @@
                 org-use-sub-superscripts "{}"
                 org-hide-emphasis-markers t
                 org-startup-with-inline-images t
-                org-image-actual-width '(300)))
+		org-src-fontify-natively t
+                org-image-actual-width '(300))
+  (custom-set-faces
+   '(org-block ((t (:background "#3c3836" :extend t))))
+   '(org-block-begin-line ((t (:background "#282828" :extend t))))
+   '(org-block-end-line ((t (:background "#282828" :extend t))))
+   '(org-meta-line ((t (:foreground "#fe8019" :extend t))))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)))))
 
 (use-package org-appear
     :hook
@@ -138,8 +152,36 @@
     (org-modern-keyword nil)
     (org-modern-checkbox nil)
     (org-modern-table nil)
-    (modus-themes-load-operandi))
+    (modus-themes-load-operandi)
+    (org-modern-todo-faces
+	  '(("DONE" . (:foreground "#3c3836" :background "#8ec07c" :weight bold)))))
 
+(use-package beacon
+  :config
+  (beacon-mode))
 
+;Start Screen, need to setup icons
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-set-navigator t)
+  (dashboard-center-content t)
+  (dashboard-display-icons-p t)
+  (dashboard-icon-type 'nerd-icons)
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons t)
+  (dashboard-image-banner-max-height 250)
+  (dashboard-startup-banner "/Users/georgemican/images-wallpapers/ue-colorful.png"))
+ 
+ 
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom
+  (doom-modeline-height 26))
+ 
+ 
 (set-face-attribute 'default nil :height 130)
 
